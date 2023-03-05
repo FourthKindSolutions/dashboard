8082c3005077371b670619167fc7348cb5a6c510
Your Name <you@example.com>
2023-03-05 17:53:53 -0600

    a

--- apps/app1.php
+++ apps/app1.php
@@ -21,37 +21,6 @@ if (isset($_SESSION['redirect'])) {
 <!DOCTYPE html>
 <html>
    <head>
-      <script type="text/javascript">
-      function resizeIframe(obj){
-         obj.style.height = 0;
-         obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
-      }
-      </script>      
-<script type="text/javascript">
-function getDocHeight(doc) {
-    doc = doc || document;
-    // stackoverflow.com/questions/1145850/
-    var body = doc.body, html = doc.documentElement;
-    var height = Math.max( body.scrollHeight, body.offsetHeight, 
-        html.clientHeight, html.scrollHeight, html.offsetHeight );
-    return height;
-}
-
-function setIframeHeight(id) {
-    var ifrm = document.getElementById(id);
-    var doc = ifrm.contentDocument? ifrm.contentDocument: 
-        ifrm.contentWindow.document;
-    ifrm.style.visibility = 'hidden';
-    ifrm.style.height = "10px"; // reset to minimal height ...
-    // IE opt. for bing/msn needs a bit added or scrollbar appears
-    ifrm.style.height = getDocHeight( doc ) + 4 + "px";
-    ifrm.style.visibility = 'visible';
-}
-
-document.getElementById('ifrm').onload = function() { // Adjust the Id accordingly
-    setIframeHeight(this.id);
-}
-</script>      
       <script>
          window.onload = function() {
             var iframe = document.getElementById("myiframe");
@@ -61,6 +30,6 @@ document.getElementById('ifrm').onload = function() { // Adjust the Id according
       </script>
    </head>
    <body>
-      <iframe onload='resizeIframe(this)' id="myiframe" src="https://4ks.mx"></iframe>
+      <iframe width="100%" height="100%" id="myiframe" src="https://4ks.mx"></iframe>
    </body>
 </html>
