2f1aae01e7b41c84891cd3a50d791d6e2a2a0294
Your Name <you@example.com>
2023-03-05 17:42:16 -0600

    a

---
+++ apps/app1.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+session_set_cookie_params(0, '/', '.4ks.online');
+
+
+// Check if the user is logged in
+if (!isset($_SESSION['username'])) {
+    // Store the current page in a session variable
+    $_SESSION['redirect'] = $_SERVER['REQUEST_URI'];
+    header('Location: /');
+    exit;
+}
+
+// User is logged in, display the app content here
+$username = $_SESSION['username'];
+
+// Clear the redirect session variable if it exists
+if (isset($_SESSION['redirect'])) {
+    unset($_SESSION['redirect']);
+}
+?>
+<!DOCTYPE html>
+<html>
+   <head>
+      <script>
+         window.onload = function() {
+            var iframe = document.getElementById("myiframe");
+            iframe.width = iframe.contentWindow.document.body.scrollWidth;
+            iframe.height = iframe.contentWindow.document.body.scrollHeight;
+         }
+      </script>
+   </head>
+   <body>
+      <iframe width="100%" height="100%" id="myiframe" src="https://4ks.mx"></iframe>
+   </body>
+</html>
