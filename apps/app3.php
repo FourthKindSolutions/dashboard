6b79b2ef12b58bfa8576c5f0e180d5ad45e12ae4
juangarcia4ks <107892265+juangarcia4ks@users.noreply.github.com>
2023-03-05 22:02:26 -0600

    Update app3.php

--- apps/app3.php
+++ apps/app3.php
@@ -65,6 +65,6 @@ if (isset($_SESSION['redirect'])) {
 		<a href="../logout.php">Logout</a>
 	</nav>
 
-	<iframe src="https://container.csn.svc.cluster.local:8443" frameborder="0"></iframe>
+	<iframe src="https://10.128.1.113:8443" frameborder="0"></iframe>
 </body>
 </html>
